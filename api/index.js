efd3e0b2da28fd8712eef8ee774aeaa40a244906
Rúben Martins <rubendavidsilvamartins@gmail.com>
2026-05-12 16:43:24 +0100

    debug: catch app load error and return JSON in serverless

--- api/index.js
+++ api/index.js
@@ -4,6 +4,13 @@
  * Importa o Express app e exporta-o como handler serverless.
  * Todas as rotas /api/* são tratadas aqui.
  */
-const app = require('../server/app');
+let app;
+try {
+  app = require('../server/app');
+} catch (err) {
+  app = (req, res) => {
+    res.status(500).json({ error: 'Failed to load app', message: err.message, stack: err.stack });
+  };
+}
 
 module.exports = app;
